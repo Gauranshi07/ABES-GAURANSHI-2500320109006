05dd3265e5a1b6d12bf9234feb72943bfc846384
Gauranshi Varshney <sandeepvarshney1974@gmail.com>
2026-09-01 22:57:44 +0530

    Add book npm.app

---
+++ CSE-11-FSD/book npm.app/book.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Book(){
+    return(
+        <div>
+            {/* <img src="" width={100} height={100} alt="Book Image" /> */}
+            <h2>Title: ReactJS</h2>
+            <h2>Price: ₹465/-</h2>
+            <button>AddToCart</button>
+        </div>
+    )
+}
+export default Book;
---
+++ CSE-11-FSD/book npm.app/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Book from "./book.jsx";
+
+function App() {
+    return (
+        <div>
+            <h2><center>My Book Store</center></h2>
+            <div>
+                <Book />
+            </div>
+        </div>
+    );
+}
+
+export default App;
